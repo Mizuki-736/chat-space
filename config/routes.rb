bfc7f98635544fc64d3b1201acba4e02b8104044
zxp736 <cod736@gmail.com>
2019-04-15 15:16:53 +0900

    メッセージ送信機能/コントローラーを書くまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'groups#index'
+  root 'messages#index'
   resource :user,  only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messarges, only: [:index, :create]
