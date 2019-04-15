ed063cf0f3edd61af48030fbbef34f4fa12ef4be
zxp736 <cod736@gmail.com>
2019-04-16 00:07:44 +0900

    グループ作成機能/問題４
    チャットメンバーのチェックボックス表示
    作成成功時、エラー発生時にフラッシュメッセージ表示
    groupモデルにvaridation設定（presence

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,30 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
+    @group.users << current_user
+    # groupを作る時にログイン中のユーザー情報を登録
   end
 
   def create
+    @group = Group.new(group_params) # private内へ
+    if @group.save #もしsaveが成功したら
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new #render,redirect_toは同じとこで２度連続で使えない
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+    # 二重ハッシュ
   end
 
 end
